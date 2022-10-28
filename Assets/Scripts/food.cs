5381ceafde3bdca74a666a99bb4297c59c869e49
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-10-28 19:10:39 +0900

    f src: food 앞으로 나가기

---
+++ Assets/Scripts/food.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0.0f, 0.5f, 0.0f);
+    }
+}
