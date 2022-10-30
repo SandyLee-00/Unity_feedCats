df7cbd604e81f25a17c061fa9e60c96a22bdb78e
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-10-30 14:52:29 +0900

    f scr: food와 cat 충돓 -> 체력바 차고 배부른 상태 바꾸기

--- Assets/Scripts/cat.cs
+++ Assets/Scripts/cat.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class cat : MonoBehaviour
 {
+    float full = 5.0f;
+    float energy = 0.0f;
+
     void Start()
     {
         float posX = Random.Range(-8.5f, 8.5f);
         float posY = 30.0f;
         transform.position = new Vector3(posX, posY, 0);
+        
     }
 
     void Update()
     {
         transform.position += new Vector3(0.0f, -0.05f, 0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Food")
+        {
+            if(energy < full)
+            {
+                energy += 1.0f;
+                Destroy(collision.gameObject);
+                gameObject.transform.Find("hungry/Canvas/bar_front").transform.localScale = new Vector3(energy / full, 1.0f, 1.0f);
+            }
+            else
+            {
+                gameObject.transform.Find("hungry").gameObject.SetActive(false);
+                gameObject.transform.Find("full").gameObject.SetActive(true);
+            }
+        }
     }
 }
